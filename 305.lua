5ff22cc480a45c12c3326ca1129307d4883dc7b4
CryWinkleSam <uncync82@gmail.com>
2026-05-31 15:31:10 -0700

    Create 305.lua

---
+++ 305.lua
@@ -0,0 +1,11 @@
+if syn and syn.queue_on_teleport then
+    syn.queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/CryWinkleSam/get/refs/heads/main/305.lua"))
+elseif queue_on_teleport then
+    queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/CryWinkleSam/get/refs/heads/main/305.lua"))
+end
+
+
+
+local me = game.Players.LocalPlayer
+
+me:Kick("ROBLOX ToS Violation(s): Exploiting. - If you think this is a mistake, Visit our website to appeal.")
