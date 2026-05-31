b2c539fadc568658c83d2d8c7d5ee61482bff7e8
CryWinkleSam <uncync82@gmail.com>
2026-05-31 15:38:13 -0700

    Update 305.lua

--- 305.lua
+++ 305.lua
@@ -1,25 +1,14 @@
 wait(1)
-
 warn("FEWare: Hooked to redlibUU")
-
 wait(3)
-
 warn("FEWare: Loading .. .. ")
-
 repeat task.wait() until game:IsLoaded()
-
 task.wait(1)
-
 warn("Started")
-
 if syn and syn.queue_on_teleport then
     syn.queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/CryWinkleSam/get/refs/heads/main/305.lua"))
 elseif queue_on_teleport then
     queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/CryWinkleSam/get/refs/heads/main/305.lua"))
 end
-
-
-
 local me = game.Players.LocalPlayer
-
 me:Kick("ROBLOX ToS Violation(s): Exploiting. - If you think this is a mistake, Visit our website to appeal.")
