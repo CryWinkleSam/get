3366acf630883c6eb96bbda793aaf6560728e1bd
CryWinkleSam <uncync82@gmail.com>
2026-05-31 16:18:59 -0700

    Update 305.lua

--- 305.lua
+++ 305.lua
@@ -1,12 +1,12 @@
 local PlaceId = game.PlaceId
 
 wait(1)
-warn("FEWare: Hooked to redlibUU")
+warn("AntiBan: Hooked to required stuff")
 wait(3)
-warn("FEWare: Loading .. .. ")
+warn("AntiBan: Loading .. .. ")
 repeat task.wait() until game:IsLoaded()
 task.wait(1)
-warn("Started")
+warn("AntiBan: loaded! You can now not be banned in Private Servers!")
 if syn and syn.queue_on_teleport then
     syn.queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/CryWinkleSam/get/refs/heads/main/305.lua"))
 elseif queue_on_teleport then
