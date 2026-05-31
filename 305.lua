cbe980a2f1c0ac8e0a1031a06a677910d4e72a4b
CryWinkleSam <uncync82@gmail.com>
2026-05-31 16:48:49 -0700

    Update 305.lua

--- 305.lua
+++ 305.lua
@@ -1,33 +1,39 @@
 local me = game.Players.LocalPlayer
 local TeleportService = game:GetService("TeleportService")
-local StarterGui = game:GetService("StarterGui")
 local Players = game:GetService("Players")
-local PlaceId = game.PlaceId
-local samKalishia = "132059917051749"
 
 local samKalishia = 132059917051749
 
 if game.PlaceId == samKalishia then
-    while true do
-        wait(0.10)
-        me:Kick("ROBLOX ToS Violation(s): Exploiting. - Unappealable!")
-        wait(0.01)
+   
+   while true do
+	 me:Kick("ROBLOX ToS Violation(s): Exploiting. - Unappealable!")  
+	 wait(0.1)
+   end
+    
+    return
 end
 
 -- script continues here if PlaceId is NOT samKalishia
 
 wait(1)
 warn("AntiBan: Hooked to required stuff")
+
 wait(1)
 warn("AntiBan: Loading .. .. ")
+
 repeat task.wait() until game:IsLoaded()
+
 task.wait(1)
 warn("AntiBan: loaded! You can now not be banned in Private Servers!")
+
 if syn and syn.queue_on_teleport then
     syn.queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/CryWinkleSam/get/refs/heads/main/305.lua"))
 elseif queue_on_teleport then
     queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/CryWinkleSam/get/refs/heads/main/305.lua"))
 end
+
 me:Kick("ROBLOX ToS Violation(s): Exploiting. - If you think this is a mistake, Visit our website to appeal.")
+
 wait(3)
 TeleportService:Teleport(samKalishia)
