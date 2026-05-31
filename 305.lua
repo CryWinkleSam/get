e7034d3fb9d26a55965fef78f7cedff1973ee5a3
CryWinkleSam <uncync82@gmail.com>
2026-05-31 15:35:12 -0700

    Update 305.lua

--- 305.lua
+++ 305.lua
@@ -1,6 +1,14 @@
+warn("FEWare: Hooked to redlibUU")
+
+wait(3)
+
+warn("FEWare: Loading .. .. ")
+
 repeat task.wait() until game:IsLoaded()
 
 task.wait(1)
+
+warn("Started")
 
 if syn and syn.queue_on_teleport then
     syn.queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/CryWinkleSam/get/refs/heads/main/305.lua"))
